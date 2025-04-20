4f23c9089d402eeee36b92909f20156200184431
daniela-park <daniela-park@outlook.com>
2025-04-20 09:56:25 -0300

    Adding useLocation in NavBar.tsx

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
+
+  const location = useLocation();
 
   return (
     <>
